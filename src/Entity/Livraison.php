6efe23c50892eed69e45f4e0dfab11a5e19236a6
selcuk.yalcin <yalcinselcuk79@gmail.com>
2023-02-03 12:07:56 +0100

    changement

---
+++ src/Entity/Livraison.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LivraisonRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LivraisonRepository::class)]
+class Livraison
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $expediteur = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $destinataire = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getExpediteur(): ?string
+    {
+        return $this->expediteur;
+    }
+
+    public function setExpediteur(string $expediteur): self
+    {
+        $this->expediteur = $expediteur;
+
+        return $this;
+    }
+
+    public function getDestinataire(): ?string
+    {
+        return $this->destinataire;
+    }
+
+    public function setDestinataire(string $destinataire): self
+    {
+        $this->destinataire = $destinataire;
+
+        return $this;
+    }
+
+}
